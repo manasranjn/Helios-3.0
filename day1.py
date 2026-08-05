fffccae2f90fe46568d60ffe052f0b9816daac8d
Manas Ranjan Bera <manasbera484@gmail.com>
2026-08-05 16:30:42 +0530

    Notes

---
+++ day1.py
@@ -0,0 +1,80 @@
+# print("Hello, everyone!")
+
+# This is a comment
+# print("Hello, everyone!")
+
+'''
+uydguyg
+er
+ewg
+werg
+wer
+'''
+
+"""
+This is a 
+multi-line comment
+"""
+
+# Variables
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+
+# Data types
+## Number Types
+# int
+# a = 10
+# b  = -10
+# print(type(a))
+# print(type(b))
+
+# float
+# a = 10.5
+# b = -10.5
+# print(type(a))
+# print(type(b))
+
+# complex
+# a = 10 + 5j
+# b = -10 + 5j
+# print(type(a))
+# print(type(b))
+
+## Boolean
+# a = True
+# b = False
+# print(type(a))
+# print(type(b))
+
+## Sequence Types
+## String
+a = "Hello"
+b = 'Hello'
+c = '23456789'
+d = "!@#$%^&*"
+e = '''
+
+This is a 
+multi-line string
+'''
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(e)
+# print(a[0])
+
+# List
+l1 = [1, 2, 3, 4, 5]
+l2 = ["Hello", "World", "Python"]
+l3 = [1,2,3,4,5, "Hello", "World", "Python", True, False, 10.5, 10 + 5j]
+# print(type(l1))
+# print(type(l2))
+# print(type(l3))
+# print(l3[4])
+# print(l3[7])
+
+# Tupple

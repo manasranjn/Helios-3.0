ed3536b2ec9739b6535da3b6e4554936be126d96
Manas Ranjan Bera <manasbera484@gmail.com>
2026-08-08 16:25:35 +0530

    string

---
+++ day4.py
@@ -0,0 +1,59 @@
+# for i in range(20):
+#     if i == 10:
+#         continue
+#     print(i)
+
+#     if i == 15:
+#         break
+
+# print("hello")
+
+
+# String and It's methods
+s = "Python is a programming language"
+# print(s[2])
+# print(s[-2])
+# print(s[2:10])
+# print(s[2:-4])
+# print(s[2:])
+# print(s[:5])
+# print(s[:])
+# print(s[2:20:2])
+# print(s[::-1])
+
+s1 = "Hello"
+s2 = "World"
+s3 = s1 + " " + s2
+# print(s3)
+
+s4 = s1 * 3
+# print(s4)
+
+# String inbuilt methods
+s = "Python is a Programming language"
+# print(s.lower())
+# print(s.upper())
+# print(s.capitalize())
+# print(s.title())
+# print(s.split("a"))
+# print(" ".join(s))
+# s1 = s.split(" ")
+# print(s1)
+# print(" ".join(s1))
+
+# print(s.replace("a", "aa"))
+# print(s)
+# print(s.strip())
+# print(s.lstrip())
+# print(s.rstrip())
+# print(s.find("o"))
+# print(s.find("ooo"))
+# print(s.rfind("o"))
+# print(s.index("oo"))
+print(s.isalpha())
+print(s.isdigit())
+print(s.isalnum())
+print(s.isdecimal())
+print(s.count("l"))
+print(s.startswith("h"))
+print(s.endswith("e"))
